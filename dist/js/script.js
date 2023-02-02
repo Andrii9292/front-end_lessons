cb89c1065b8e8ee939350f4f4f89b2ccadc2d2bc
Andrii9292 <119946292+Andrii9292@users.noreply.github.com>
2023-02-03 06:37:58 +0900

    .

--- dist/js/script.js
+++ dist/js/script.js
@@ -6,3 +6,20 @@ $(document).ready(function () {
         adaptiveHeight: true
     });
 });
+
+$(document).ready(function () {
+    $("#link-color").addClass("color-red");
+});
+
+$("#link-color").click(function () {
+    $(this).addClass("color-red");
+    $("#link-color2, #link-color3").removeClass("color-red");
+});
+$("#link-color2").click(function () {
+    $(this).addClass("color-red");
+    $("#link-color, #link-color3").removeClass("color-red");
+});
+$("#link-color3").click(function () {
+    $(this).addClass("color-red");
+    $("#link-color, #link-color2").removeClass("color-red");
+});
