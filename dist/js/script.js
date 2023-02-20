6a1ad3956c38726904a5481563d2e779727763a6
Andrii9292 <119946292+Andrii9292@users.noreply.github.com>
2023-02-21 00:52:35 +0900

    .

---
+++ dist/js/script.js
@@ -0,0 +1,11 @@
+$('.navi li').mouseenter(function () {
+    var changeImage = $(this).attr('data-image')
+    $('.photo').css({
+        'background-image': 'url(' + changeImage + ')'
+    })
+})
+$('.navi li').mouseleave(function () {
+    $('.photo').css({
+        'background-image': ''
+    })
+})
