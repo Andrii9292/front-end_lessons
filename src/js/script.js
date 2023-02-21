ce4bebce73097e8021d574b383e237e6dd56c08f
Andrii9292 <119946292+Andrii9292@users.noreply.github.com>
2023-02-22 00:08:18 +0900

    .

--- src/js/script.js
+++ src/js/script.js
@@ -21,9 +21,15 @@ $(document).ready(function () {
     $('#click').click(function () {
         $('#menu-list').toggleClass('active');
     });
-});
 
-$(document).ready(function () {
+    $(document).click(function (event) {
+        var menu = $('#menu-list');
+        var menuToggle = $('#click');
+        if (!menu.is(event.target) && !menuToggle.is(event.target) && menu.has(event.target).length === 0) {
+            menu.removeClass('active');
+        }
+    });
+
     $('.has-sub').click(function (event) {
         event.stopPropagation();
         $(this).children('ul').toggleClass('active');
