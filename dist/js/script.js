9d6d6dded346ecbe47237c98db32c810556b3066
Andrii9292 <119946292+Andrii9292@users.noreply.github.com>
2023-02-22 20:30:46 +0900

    .

--- dist/js/script.js
+++ dist/js/script.js
@@ -31,13 +31,14 @@ $(document).ready(function () {
 });
 
 $(document).ready(function () {
-    $('.grid').masonry({
-        itemSelector: '.grid-item',
-        columnWidth: 200,
-        gutter: 20
+    $('.grid').imagesLoaded(function () {
+        $('.grid').masonry({
+            itemSelector: '.grid-item',
+            columnWidth: 200,
+            gutter: 20
+        });
     });
 });
-
 
 const smoothScrollLinks = document.querySelectorAll('.smooth-scroll');
 
